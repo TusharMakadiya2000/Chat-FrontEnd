a18a784292d7701eaad813cd53493c0e371097ec
Tushar <tusharmakadiya2000@gmail.com>
2025-01-04 00:14:02 +0530

    Fix: api changes.

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -25,7 +25,7 @@ import ProfileUserListProvider from "./ProfileUserList";
 import { JwtPayload, jwtDecode } from "jwt-decode";
 import io from "socket.io-client";
 
-const socket = io("http://192.168.1.10:5000");
+const socket = io("https://chat-bachend.onrender.com");
 
 // Inherite userId in JwtPayload interface
 interface UserIdJwtPayload extends JwtPayload {
